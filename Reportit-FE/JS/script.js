06fac72f961a2f9db8064a68e54f2f85a7e0bcd3
AsafAce <asaf93ya@gmail.com>
2019-04-26 17:05:31 +0300

    json

--- Reportit-FE/JS/script.js
+++ Reportit-FE/JS/script.js
@@ -1,61 +1,52 @@
-//Accordian Action
-var action = 'click';
-var speed = "500";
+//Google map
 
+var map, infoWindow,pos,data;
 
-//Document.Ready
-$(document).ready(function(){
-  //Question handler
-$('li.q').on(action, function(){
-  //gets next element
-  //opens .a of selected question
-$(this).next().slideToggle(speed)
-    //selects all other answers and slides up any open answer
-    .siblings('li.a').slideUp();
-  
-  //Grab img from clicked question
-var img = $(this).children('img');
-  //Remove Rotate class from all images except the active
-  $('img').not(img).removeClass('rotate');
-  //toggle rotate class
-  img.toggleClass('rotate');
-
-});//End on click
-
-
-});//End Ready
-
-
-/*
-let reportData =
-{
-  reporterName = undefined,
-  reporterPhone = undefined,
-  reporterEmail = undefined,
-  reportLocation = 
-  {
-    latitude = undefined,
-    longitude = undefined
-  },
-  reportpicture = undefined
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: -34.397,
+            lng: 150.644
+        },
+        zoom: 16
+    });
+    infoWindow = new google.maps.InfoWindow;
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            //reportData.reportLocation.latitude = pos.lat
+            
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('&nbsp &nbsp &nbsp &nbsp 📍ReportIt Location📍');
+            infoWindow.open(map);
+            map.setCenter(pos);
+        }, function () {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
 }
 
-//update:
-document.getElementById("reportForm").addEventListener('submit', () =>
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+}
+//Google map
+
+// Make json
+function onSubmit( form )
 {
-  //validation, validator (npm)
-  //image size < 1.5 M
-  //resize image with sharp (npm)
-  
-  //fill report data:
-  reportData.reporterName = document.getElementById("reporterName").value
-  //the rest of data
-  
-
-
-  //when all data is ready in reportData
-  //http request (POST) to send all JSON data
-  //http request (POST) upload picture
-  //error handler = if response is error
-})
-*/
+  data = JSON.stringify( $(form).serializeArray()); 
+  data += JSON.stringify(pos);
+  console.log( data );
+  document.form.submit();
+  return false; //don't submit
+}
