17fa9973fa334a1b478e6b4fa90f124fd257ffa6
batel <batelm14@gmail.com>
2019-05-07 14:53:47 +0300

    navbar

--- Reportit-FE/JS/script.js
+++ Reportit-FE/JS/script.js
@@ -30,6 +30,7 @@ function initMap() {
         // Browser doesn't support Geolocation
         handleLocationError(false, infoWindow, map.getCenter());
     }
+    
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
